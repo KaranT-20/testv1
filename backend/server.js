6186dea7cfca664d154129d94be728a9db0f7167
KaranT-20 <notkaran20@gmail.com>
2026-09-26 00:28:30 +0530

    update server to listen on 0.0.0.0 with port fallback 5000

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ import { app } from './app.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`server is running at http://localhost:${PORT}`)
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server listening on port ${PORT}`);
+});
